e4369363a04aa81e7606f8d58a79076a7cc75b77
Helper <piyushyadv17147714@gmail.com>
2025-08-26 18:12:50 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# === Config File ===
+# Sab variables already fill hai, ab aur kuch change karne ki zarurat nahi hai.
+
+API_ID = 20491966
+API_HASH = "aa1c8f86db7f78fe9bfdd77bb48a5b23"
+BOT_TOKEN = "8139952900:AAF5Gk4l9xnx5OmaEeEr-zK5sPaUJ47hNAM"
+OWNER_ID = 6876826498
